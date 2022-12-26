5759fe729a2b7b4020b7664bc70fba1a6a25b8ea
KienDev <ngotrungkien2k2@gmail.com>
2022-12-26 15:22:36 +0700

    Code splitting Routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import { Fragment } from "react";
+//lazy, Suspense: Code splitting Routes: mỗi page thì load page đó, ko load page ko liên quan
+import { Fragment, lazy, Suspense } from "react";
 // NavLink: link có active
 import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
 
 import Main from "~/components/layout/Main";
-import MovieDetailPage from "~/components/movie/MovieDetailPage";
-import HomePage from "~/components/pages/HomePage";
-import MoviePage from "~/components/pages/MoviePage";
+
+const MovieDetailPage = lazy(() =>
+    import("~/components/movie/MovieDetailPage")
+);
+const HomePage = lazy(() => import("~/components/pages/HomePage"));
+const MoviePage = lazy(() => import("~/components/pages/MoviePage"));
 
 function App() {
     return (
         <Fragment>
-            <Routes>
-                {/* ko có path và la cha của HomePage để page nào cũng có header là main */}
-                <Route element={<Main></Main>}>
-                    {/* Outlet: bên main là cha để hiển thị component HomePage: trang chủ */}
-                    <Route path="/" element={<HomePage></HomePage>}></Route>
+            <Suspense fallback={<></>}>
+                <Routes>
+                    {/* ko có path và la cha của HomePage để page nào cũng có header là main */}
+                    <Route element={<Main></Main>}>
+                        {/* Outlet: bên main là cha để hiển thị component HomePage: trang chủ */}
+                        <Route path="/" element={<HomePage></HomePage>}></Route>
 
-                    {/* page movies */}
-                    <Route
-                        path="/movie"
-                        element={<MoviePage></MoviePage>}
-                    ></Route>
-                    <Route
-                        path="/movie/:movieId"
-                        element={<MovieDetailPage></MovieDetailPage>}
-                    ></Route>
-                </Route>
-            </Routes>
+                        {/* page movies */}
+                        <Route
+                            path="/movie"
+                            element={<MoviePage></MoviePage>}
+                        ></Route>
+                        <Route
+                            path="/movie/:movieId"
+                            element={<MovieDetailPage></MovieDetailPage>}
+                        ></Route>
+                    </Route>
+                </Routes>
+            </Suspense>
         </Fragment>
     );
 }
