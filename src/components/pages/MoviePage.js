9a44a71f70799924e8cc5a91edce9a0dada56f3e
KienDev <ngotrungkien2k2@gmail.com>
2022-12-26 10:16:10 +0700

    Thêm chức năng phân trang - sử dụng react-paginate

--- src/components/pages/MoviePage.js
+++ src/components/pages/MoviePage.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
+import ReactPaginate from "react-paginate";
 
 import { fetcher } from "../../config";
 import MovieCard from "../movie/MovieCard";
 import useDebounce from "../hooks/useDebounce";
 
+// one page have 20 cards movie
+const itemsPerPage = 20;
 const MoviePage = () => {
+    // page mỗi phân trang 1,2,3,...
+    const [nextPage, setNextPage] = useState(1);
     const [filter, setFilter] = useState("");
     const [url, setUrl] = useState(
-        `https://api.themoviedb.org/3/movie/popular?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200`
+        `https://api.themoviedb.org/3/movie/popular?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200&page=${nextPage}`
     );
     // 500 seconds sẽ thay thế là change input
     const filterDebounce = useDebounce(filter, 500);
@@ -17,20 +22,37 @@ const MoviePage = () => {
     };
 
     const { data, error } = useSWR(url, fetcher);
-    const movies = data?.results || [];
     const loading = !data && !error;
 
+    const movies = data?.results || [];
+    // const { page, total_pages } = data;
     useEffect(() => {
         if (filterDebounce) {
             setUrl(
-                `https://api.themoviedb.org/3/search/movie?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200&query=${filterDebounce}`
+                `https://api.themoviedb.org/3/search/movie?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200&query=${filterDebounce}&page=${nextPage}`
             );
         } else {
             setUrl(
-                `https://api.themoviedb.org/3/movie/popular?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200`
+                `https://api.themoviedb.org/3/movie/popular?api_key=6e1eb12ab3735cf3feb3ab8c6dc7b200&page=${nextPage}`
             );
         }
-    }, [filterDebounce]);
+    }, [filterDebounce, nextPage]);
+
+    // react-paginate
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    //tính total count
+    useEffect(() => {
+        if (!data || !data.total_results) return;
+        setPageCount(Math.ceil(data.total_results / itemsPerPage));
+    }, [data, itemOffset]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.total_results;
+        setItemOffset(newOffset);
+        setNextPage(event.selected + 1);
+    };
 
     return (
         <div className="px-20 py-10">
@@ -72,42 +94,17 @@ const MoviePage = () => {
                     ))}
             </div>
 
-            <div className="flex items-center justify-center gap-x-5">
-                <span className="cursor-pointer">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.75 19.5L8.25 12l7.5-7.5"
-                        />
-                    </svg>
-                </span>
-                <span className="inline-block px-4 py-2 leading-none bg-white rounded-sm cursor-pointer text-slate-900">
-                    1
-                </span>
-                <span className="cursor-pointer">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                        />
-                    </svg>
-                </span>
+            <div className="mt-10">
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+                    className="pagination"
+                />
             </div>
         </div>
     );
